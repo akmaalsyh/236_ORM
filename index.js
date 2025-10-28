fb466150ae3d602f56b86e06a3cfc44d860fa4cc
akmaalsyh <taufansyah50@gmail.com>
2025-10-28 10:12:39 +0700

    menjalankan server setelah sinkronisasi database berhasil

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ app.listen(port, async () => {
     console.log('Server started on port 3000');
 });
 
+db.sequelize.sync()
+// Sinkronisasi model Sequelize dengan database (membuat tabel jika belum ada)
+.then(() => {
+app.listen(3000, () => {
+    // Menjalankan server Express di port 3000 setelah database siap
+    console.log('Server started');
+    // Menampilkan pesan di console bahwa server berhasil dijalankan
+    });
+})
+.catch((err) => {
+    // Menangani error jika terjadi kegagalan sinkronisasi database
+    console.log(err);
+});
