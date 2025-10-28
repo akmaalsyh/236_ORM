13f8ed2d02b8632326fb44f498a12cca26a874ce
akmaalsyh <taufansyah50@gmail.com>
2025-10-28 09:56:06 +0700

    memberi informasi terkait sudah dimulainya server di port 3000

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const db = require('./models');
 const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, async () => {
+    console.log('Server started on port 3000');
+});
+
